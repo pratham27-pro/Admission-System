2851f6afbbcd35ad9d78674642630ca274e8e92f
fawahkhan <khanmohdfawah21@gmail.com>
2025-04-13 23:06:29 +0530

    Landing page done

---
+++ frontend/admission management/src/Components/Dashboard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Dashboard() {
+  return (
+    <div className="flex-grow bg-white">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-20">
+        <h1 className="text-4xl font-bold text-gray-900 mb-8">Dashboard</h1>
+        <p className="text-gray-600 leading-relaxed">
+         Hi Sachin this is just a rough page for admin dashboard. since you were making it thus its on you to link it here or delete this file and make the changes accordingly.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Dashboard
